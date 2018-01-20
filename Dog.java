2f31fe7cd53e9ea17d9e21c664b0bf2e4085a41a
Kent Collins <kent.r.collins@gmail.com>
2018-01-20 10:19:57 +0000

    added classes

---
+++ Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Pet {
+	
+	public Dog(String name) {
+		// implementation not shown to student
+		super(name);
+	}
+
+	public String speak() {
+		// implementation not shown to student
+		return "woof";
+	}
+}
